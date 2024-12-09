aeb175a64710797e5f79a61d6c0e80ee59e55a22
Barry Mamadou Siradio <87446001+barry156@users.noreply.github.com>
2024-12-09 11:14:43 +0100

    add  a button wich reads  the text

--- app/src/main/java/com/example/trainxpert/screens/ActivityDetailScreen.kt
+++ app/src/main/java/com/example/trainxpert/screens/ActivityDetailScreen.kt
@@ -1,55 +1,28 @@
 package com.example.trainxpert.screens
 
-import android.annotation.SuppressLint
+import android.speech.tts.TextToSpeech
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.trainxpert.components.HistoryCard
 import com.example.trainxpert.model.ActivityItem
-import com.example.trainxpert.model.SportSession
 import com.example.trainxpert.ui.theme.CardTitle
-import com.example.trainxpert.ui.theme.MainGreyBackground
-import com.example.trainxpert.viewmodels.LocalSportSessionViewModel
-import ufr.mim.netfloux.components.RawButton
-
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -57,6 +30,26 @@ fun ActivityDetailScreen(
     backAction: () -> Unit,
     activity: ActivityItem
 ) {
+    val context = LocalContext.current // Obtenez le contexte local
+
+    // Déclarez TTS comme une variable mutable à l'intérieur de remember
+    var tts: TextToSpeech? by remember { mutableStateOf(null) }
+    tts = TextToSpeech(context) { status ->
+        if (status == TextToSpeech.SUCCESS) {
+            tts?.language = Locale.getDefault() // Configurez la langue
+        }
+    }
+
+
+
+    // Disposez du TTS lors de la sortie de ce composable
+    DisposableEffect(Unit) {
+        onDispose {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -134,8 +127,6 @@ fun ActivityDetailScreen(
                             style = TextStyle(
                                 fontWeight = FontWeight.SemiBold,
                                 fontSize = 18.sp,
-                                //color = Color(0xFF6200EE)
-                                // Couleur personnalisée
                                 color = CardTitle
                             )
                         )
@@ -153,7 +144,7 @@ fun ActivityDetailScreen(
                             style = TextStyle(
                                 fontWeight = FontWeight.SemiBold,
                                 fontSize = 18.sp,
-                                color = CardTitle // Couleur personnalisée
+                                color = CardTitle
                             )
                         )
                         Spacer(modifier = Modifier.height(8.dp))
@@ -161,6 +152,17 @@ fun ActivityDetailScreen(
                             text = activity.conseil,
                             style = TextStyle(fontSize = 14.sp, color = Color.Gray)
                         )
+
+                        // Bouton pour lire le texte si la catégorie est "Conseil"
+                        if (activity.category == "Conseil") {
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Button(onClick = {
+                                val textToRead = "Pratique: ${activity.pratique}. Conseil: ${activity.conseil}"
+                                tts!!.speak(textToRead, TextToSpeech.QUEUE_FLUSH, null, null)
+                            }) {
+                                Text(text = "Lire le texte")
+                            }
+                        }
                     }
                 }
             }
