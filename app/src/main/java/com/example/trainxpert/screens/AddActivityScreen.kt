ff0a13057b1d242282a5d897fe766e6d2ab8d413
adleu <audia.adel@gmail.com>
2024-12-09 09:41:01 +0100

    +style addActivity butoon

--- app/src/main/java/com/example/trainxpert/screens/AddActivityScreen.kt
+++ app/src/main/java/com/example/trainxpert/screens/AddActivityScreen.kt
@@ -1,19 +1,34 @@
 package com.example.trainxpert.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,8 +39,7 @@ import com.example.trainxpert.components.DateTimePickerField
 import com.example.trainxpert.model.ActivityItem
 import com.example.trainxpert.ui.theme.BottomBarHeight
 import com.example.trainxpert.ui.theme.ButtonColor
-import com.example.trainxpert.ui.theme.CardTitle
-import com.example.trainxpert.ui.theme.MainPadding
+import com.example.trainxpert.ui.theme.ButtonTextSize
 import com.example.trainxpert.ui.theme.formBackground
 import java.time.LocalDateTime
 
@@ -66,7 +80,6 @@ fun AddActivityScreen(
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            // Contenu principal
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -74,7 +87,6 @@ fun AddActivityScreen(
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                // Activité sélectionnée
                 TextField(
                     value = selectedActivity?.title ?: "",
                     onValueChange = {},
@@ -91,7 +103,10 @@ fun AddActivityScreen(
                 DropdownMenu(
                     expanded = dropdownExpanded,
                     onDismissRequest = { dropdownExpanded = false },
-                    modifier = Modifier.fillMaxWidth().background(formBackground).align(Alignment.CenterHorizontally)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(formBackground)
+                        .align(Alignment.CenterHorizontally)
                 ) {
                     activities.forEach { activity ->
                         DropdownMenuItem(
@@ -113,6 +128,27 @@ fun AddActivityScreen(
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
+//
+//                Button(
+//                    onClick = {
+//                    },
+//                    modifier = Modifier
+//                        .padding(16.dp)
+//                        .fillMaxWidth(),
+//                    colors = ButtonColors(formBackground, Color.Black, Color.White, Color.White),
+//                    shape = RoundedCornerShape(5.dp),
+//                    contentPadding = PaddingValues(MainPadding)
+//
+//
+//                ) {
+//                    TextField(
+//                        value = duration,
+//                        onValueChange = { duration = it },
+//                        label = { Text("Durée (min)") },
+//                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+//                        modifier = Modifier.fillMaxWidth()
+//                    )
+
 
                 // Champs Durée, Distance, Calories
                 TextField(
@@ -130,7 +166,9 @@ fun AddActivityScreen(
                     onValueChange = { distance = it },
                     label = { Text("Distance (km)") },
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
@@ -144,11 +182,10 @@ fun AddActivityScreen(
                 )
             }
 
-            // Boutons alignés en bas de l'écran
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .align(Alignment.BottomCenter) // Aligne la colonne en bas du Box
+                    .align(Alignment.BottomCenter)
                     .padding(16.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
@@ -172,9 +209,16 @@ fun AddActivityScreen(
                         }
                     },
                     colors = ButtonDefaults.buttonColors(ButtonColor),
-                    modifier = Modifier.fillMaxWidth().padding(bottom = BottomBarHeight)
+                    modifier = Modifier
+                        .padding(bottom = BottomBarHeight)
+                        .align(Alignment.CenterHorizontally)
+                        .fillMaxWidth(0.5f)
                 ) {
-                    Text("Enregistrer", color = Color.White)
+                    Text(
+                        "Enregistrer",
+                        color = Color.White,
+                        fontSize = ButtonTextSize
+                    )
                 }
             }
         }
