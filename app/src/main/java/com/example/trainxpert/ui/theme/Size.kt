f1fc6dd37a5b1ba36ca3c8eb7600333344591128
adleu <audia.adel@gmail.com>
2024-12-10 09:42:01 +0100

    +style mainPadding

--- app/src/main/java/com/example/trainxpert/ui/theme/Size.kt
+++ app/src/main/java/com/example/trainxpert/ui/theme/Size.kt
@@ -3,11 +3,11 @@ package com.example.trainxpert.ui.theme
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-val MainPadding = 8.dp
+val MainPadding = 16.dp
 val TitleCardFontSize = 18.sp
 
 //History
-val HistoryCardFontSize = 15.sp
+val HistoryCardFontSize = 18.sp
 val HistoryCardPadding = 5.dp
 val ButtonTextSize = 18.sp
 val BottomBarHeight = 100.dp
