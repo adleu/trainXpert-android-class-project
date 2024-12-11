846c44e3015d53c0a10f236ab6196123c6604269
adleu <audia.adel@gmail.com>
2024-12-11 14:01:14 +0100

    +feat edition in sessionDetailsCard

--- app/src/main/java/com/example/trainxpert/components/SessionDetailsCard.kt
+++ app/src/main/java/com/example/trainxpert/components/SessionDetailsCard.kt
@@ -3,49 +3,47 @@ package com.example.trainxpert.components
 import android.app.AlertDialog
 import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.sharp.Delete
 import androidx.compose.material.icons.sharp.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.trainxpert.model.SportSession
-import com.example.trainxpert.navigation.NavigationHistoryComponent
-import com.example.trainxpert.screens.HistoryDetailScreen
-import com.example.trainxpert.ui.theme.BackgroundCalendarCurrentDay
+import com.example.trainxpert.ui.theme.ButtonColor
 import com.example.trainxpert.ui.theme.CardTitle
-import com.example.trainxpert.ui.theme.HistoryCardFontSize
 import com.example.trainxpert.ui.theme.MainPadding
-import com.example.trainxpert.ui.theme.TitleCardFontSize
 import com.example.trainxpert.viewmodels.LocalSportSessionViewModel
 import java.time.format.TextStyle
 import java.util.Locale
@@ -55,6 +53,8 @@ import java.util.Locale
 fun SessionDetailsCard(navController: NavController, session: SportSession) {
     val viewModel = LocalSportSessionViewModel.current
     val context = LocalContext.current
+
+    var showDialog by remember { mutableStateOf(false) }
 
 
     val builder = AlertDialog.Builder(context)
@@ -77,7 +77,8 @@ fun SessionDetailsCard(navController: NavController, session: SportSession) {
 //            .padding(MainPadding, MainPadding, MainPadding, bottom = 150.dp),
             .padding(MainPadding),
         shape = RoundedCornerShape(8.dp),
-    ) {
+
+        ) {
 
         Column(
             modifier = Modifier
@@ -86,71 +87,66 @@ fun SessionDetailsCard(navController: NavController, session: SportSession) {
                 .fillMaxWidth()
 //                .fillMaxHeight()
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ){
-                Text(
-                    text = session.dateTime.dayOfMonth.toString() +
-                            " ${
-                                session.dateTime.month.getDisplayName(TextStyle.FULL, Locale.FRENCH)
-                                    .replaceFirstChar { it.uppercase() }
-                            }" +
-                            " ${session.dateTime.year}",
+            Row {
+                Text(text = session.dateTime.dayOfMonth.toString() + " ${
+                    session.dateTime.month.getDisplayName(TextStyle.FULL, Locale.FRENCH)
+                        .replaceFirstChar { it.uppercase() }
+                }" + " ${session.dateTime.year}  ",
                     fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier.padding(top = 10.dp))
+
+
+//                Spacer(modifier = Modifier.weight(1f))
+                Row {
+                    IconButton(onClick = {
+                        showDialog = true
+                    }) {
+                        Icon(
+                            imageVector = Icons.Sharp.Edit,
+                            contentDescription = "Edit",
+//                        modifier = Modifier.weight(1f)
+                        )
+                    }
+
+                    IconButton(onClick = {
+                        dialog.show()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Sharp.Delete,
+                            contentDescription = "Delete",
+//                        modifier = Modifier.weight(1f)
+                        )
+                    }
+                }
+
+
+            }
+            Row {
+
+                Text(
+                    text = session.dateTime.hour.toString() + "h" + session.dateTime.minute.toString()
+                        .padStart(2, '0'),
+                    fontSize = 20.sp,
                     fontWeight = FontWeight.Bold,
                     color = Color.Black,
                     modifier = Modifier.padding(top = 10.dp)
                 )
-
-//                IconButton(onClick = {
-//
-//                    viewModel.deleteSession(session)
-//                    navController.popBackStack()
-////                NavigationHistoryComponent(Modifier)
-//
-//                }) {
-//                    Icon(
-//                        imageVector = Icons.Sharp.Edit,
-//                        contentDescription = "Edit",
-////                        modifier = Modifier.weight(1f)
-//                    )
-//                }
-
-                IconButton(onClick = {
-                    dialog.show()
-//                    viewModel.deleteSession(session)
-//                    navController.popBackStack()
-//                NavigationHistoryComponent(Modifier)
-
-                }) {
-                    Icon(
-                        imageVector = Icons.Sharp.Delete,
-                        contentDescription = "Delete",
-//                        modifier = Modifier.weight(1f)
-                    )
-                }
-
             }
-            Text(
-                text = session.dateTime.hour.toString() + "h" + session.dateTime.minute.toString(),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black,
-                modifier = Modifier.padding(top = 10.dp)
-            )
+
 
 
             Spacer(modifier = Modifier.height(50.dp))
+            Row {
+                Text(
+                    text = session.activityName,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                )
+            }
 
-            Text(
-                text = session.activityName,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black,
-//                modifier = Modifier.fillMaxWidth().fillMaxHeight()
-            )
 
             Spacer(modifier = Modifier.height(10.dp))
 
@@ -168,13 +164,19 @@ fun SessionDetailsCard(navController: NavController, session: SportSession) {
                 modifier = Modifier.fillMaxWidth()
             )
 
-            session.caloriesBurned?.let {
+            session.caloriesBurned?.takeIf { it != 0 }?.let {
+
+
                 Spacer(modifier = Modifier.height(10.dp))
 
                 Text(
                     text = buildAnnotatedString {
                         append("Vous avez brûlé ")
-                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = CardTitle)) {
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Bold, color = CardTitle
+                            )
+                        ) {
                             append(session.caloriesBurned.toString() + " Kcal")
                         }
                         append(".")
@@ -188,13 +190,17 @@ fun SessionDetailsCard(navController: NavController, session: SportSession) {
 
 
 
-            session.distanceInKm?.let {
+            session.distanceInKm?.takeIf { it != 0.0 }?.let {
                 Spacer(modifier = Modifier.height(10.dp))
 
                 Text(
                     text = buildAnnotatedString {
                         append("Vous avez parcouru ")
-                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = CardTitle)) {
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Bold, color = CardTitle
+                            )
+                        ) {
                             append(session.distanceInKm.toString() + " Kcal")
                         }
                         append(".")
@@ -208,5 +214,83 @@ fun SessionDetailsCard(navController: NavController, session: SportSession) {
         }
 
 
+    }
+    var newActivityName by remember { mutableStateOf(session.activityName) }
+    var newDuration by remember { mutableStateOf(session.durationInMinutes) }
+    var newDistance by remember { mutableStateOf(session.distanceInKm) }
+    var newCalories by remember { mutableStateOf(session.caloriesBurned) }
+    var newDateTime by remember { mutableStateOf(session.dateTime) }
+    if (showDialog) {
+        AlertDialog(onDismissRequest = { showDialog = false },
+            title = { Text("Edition de la session") },
+            text = {
+                Column {
+                    Text("Veuillez entrer vos modifications :")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    TextField(value = newActivityName,
+                        onValueChange = { newActivityName = it },
+                        label = { Text("Nom de l'activité") },
+                        singleLine = true
+                    )
+                    TextField(
+                        value = newDuration.toString(),
+                        onValueChange = { newDuration = it.toIntOrNull() ?: 0 },
+                        label = { Text("Durée (min)") },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    )
+                    TextField(
+                        value = newDistance.toString(),
+                        onValueChange = { newDistance = it.toDoubleOrNull() ?: 0.0 },
+                        label = { Text("Distance (km)") },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    )
+                    TextField(
+                        value = newCalories.toString(),
+                        onValueChange = { newCalories = it.toIntOrNull() ?: 0 },
+                        label = { Text("Calories (kcal)") },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    )
+                    DateTimePickerField(
+                        session.dateTime,
+                        onDateTimeSelected = { newDateTime = it },
+                        1
+                    )
+                }
+            },
+            confirmButton = {
+                Button(colors = ButtonColors(
+                    ButtonColor, Color.White, ButtonColor, ButtonColor
+                ), onClick = {
+                    if (newActivityName.isNotBlank()) {
+                        // Ajouter l'activité ou effectuer une action
+                        println("session modifiée : $newActivityName")
+                        viewModel.updateSportSession(
+                            session.copy(
+                                activityName = newActivityName,
+                                durationInMinutes = newDuration,
+                                distanceInKm = newDistance,
+                                caloriesBurned = newCalories,
+                                dateTime = newDateTime
+                            ),
+
+                            )
+                        showDialog = false
+                    }
+                }) {
+                    Text("Valider")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    newActivityName = session.activityName
+                    newDuration = session.durationInMinutes
+                    newDistance = session.distanceInKm ?: 0.0
+                    newCalories = session.caloriesBurned
+                    newDateTime = session.dateTime
+                    showDialog = false
+                }) {
+                    Text("Annuler")
+                }
+            })
     }
 }
